9466a1849bc0752f13807f36b6f08832a183fdf4
Denlyl <deny1skz@gmail.com>
2024-11-26 21:28:58 +0300

    Add files via upload

--- module_7_2.py
+++ module_7_2.py
@@ -1,13 +1,13 @@
 def custom_write(file_name, strings):
-    strings = 0
+    s = 0
     strings_positions = {}
+    file = open(file_name, 'a', encoding='utf-8')
     for i in info:
-        file = open(file_name, 'a', encoding='utf-8')
         tell = (file.tell())
-        strings += 1
+        s += 1
         file.write(f'{i}\n')
-        file.close()
-        strings_positions.update({(strings, tell) : i})
+    file.close()
+    strings_positions.update({(s, tell) : i})
     return strings_positions
 
 info = [
